03824e5f4ee9ba9c198afea38841ac30d17e7f0a
Christi Savino <c.savino@ymail.com>
2016-10-15 06:38:52 -0400

    added seed data and styling

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,32 @@
+var friends = [ {
+    "name" : "Josh",
+    "photo" : "https://",
+    "scores" : [5,5,2,3,1,5,2,1,3,5]
+}, {
+    "name" : "Kirill",
+    "photo" : "https://",
+    "scores" : [1,5,1,1,1,1,1,1,5,5]
+}, {
+    "name" : "Zob",
+    "photo" : "https://",
+    "scores" : [5,3,2,3,2,5,1,3,3,4]
+},    {
+    "name" : "Gavin",
+    "photo" : "https://",
+    "scores" : [5,5,2,3,1,5,2,1,3,5]
+}, {
+    "name" : "Andre",
+    "photo" : "https://",
+    "scores" : [1,5,1,1,1,1,1,1,5,5]
+}, {
+    "name" : "PJ",
+    "photo" : "https://",
+    "scores" : [5,3,2,3,2,5,1,3,3,4]
+}, {
+    "name" : "Omar",
+    "photo" : "https://",
+    "scores" : [5,3,2,3,2,5,1,3,3,4]
+}];
+
+
+module.exports = friends;
