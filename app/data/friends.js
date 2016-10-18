b9412b1bc886b6a526cfdefb2891509f5d9ec160
Christi Savino <c.savino@ymail.com>
2016-10-18 16:04:30 -0400

    check for name and photo input

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friends = [ {
     "name" : "Josh",
-    "photo" : "http://hbz.h-cdn.co/assets/16/15/hbz-hot-guys-on-instagram-02.jpg",
+    "photo" : "http://www.mens-hairstylists.com/wp-content/uploads/2015/10/stylish-haircuts-for-men.jpg",
     "scores" : [5,4,2,5,1,1,2,1,3,5]
 }, {
     "name" : "Sherri",
@@ -36,7 +36,7 @@ var friends = [ {
     "scores" : [1,5,1,3,5,1,4,3,2,1]
 }, {
     "name" : "Krissy",
-    "photo" : "https://www.careergirls.org/sites/default/files/styles/cg_grid_images/public/Majora%20Carter%20CareerGirls%20n%20405w%20x%20405h.jpg?itok=o9aa19LG",
+    "photo" : "http://1.bp.blogspot.com/-FMhfNM7lFG4/Ud8XSJsqgLI/AAAAAAAAAIs/uyMyWoJUEk0/s1600/women-medium-wavy-hairstyles.jpg",
     "scores" : [1,3,3,1,2,4,4,1,3,1]
 }, {
     "name" : "Zob",
@@ -44,7 +44,7 @@ var friends = [ {
     "scores" : [5,2,4,3,3,2,1,3,3,4]
 }, {
     "name" : "Danielle",
-    "photo" : "http://benhairstyle.com/wp-content/uploads/2014/10/beautiful_hairstyles_for_young_women.jpgg",
+    "photo" : "http://benhairstyle.com/wp-content/uploads/2014/10/beautiful_hairstyles_for_young_women.jpg",
     "scores" : [1,1,1,1,1,1,1,1,1,1]
 }, {
     "name" : "Charu",
@@ -52,7 +52,7 @@ var friends = [ {
     "scores" : [2,3,5,5,3,5,2,1,3,2]
 }, {
     "name" : "Omar",
-    "photo" : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRnzotApUuOO77gBgsFMuUVJi375V9UCCYTjUjmAnTR45GKkQCLHg",
+    "photo" : "http://harvardsol.com/wp-content/uploads/2016/07/impressive-new-haircuts-for-young-men-all-inspirational-article.jpg",
     "scores" : [2,3,5,5,3,5,2,1,3,2]
 }];
 
